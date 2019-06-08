3f4a83c963553451a000ab101306a8cdd1f93060
jianhua.shi <jianhua.shi@pendanaan.com>
2019-06-08 23:07:52 +0800

    改变NoScrollListView 制造冲突

--- app/src/main/java/com/sjh/basecommon/widgets/NoScrollListView.java
+++ app/src/main/java/com/sjh/basecommon/widgets/NoScrollListView.java
@@ -5,14 +5,16 @@ import android.widget.ListView;
 public class NoScrollListView extends ListView {
 
     public NoScrollListView(android.content.Context context, android.util.AttributeSet attrs){
-        super(context, attrs);
+        super(context, attrs)
+
+        ;
     }
+
     /**
      * 设置不滚动
      */
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
-                MeasureSpec.AT_MOST);
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
         super.onMeasure(widthMeasureSpec, expandSpec);
 
     }
