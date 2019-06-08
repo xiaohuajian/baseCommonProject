8fb25bbc3233f61d5d8797232b3b005a54b4ce7d
jianhua.shi <jianhua.shi@pendanaan.com>
2019-06-08 22:17:08 +0800

    添加不能滚动的listview控件

---
+++ app/src/main/java/com/sjh/basecommon/widgets/NoScrollListView.java
@@ -0,0 +1,19 @@
+package com.sjh.basecommon.widgets;
+
+import android.widget.ListView;
+
+public class NoScrollListView extends ListView {
+
+    public NoScrollListView(android.content.Context context, android.util.AttributeSet attrs){
+        super(context, attrs);
+    }
+    /**
+     * 设置不滚动
+     */
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+
+    }
+}
